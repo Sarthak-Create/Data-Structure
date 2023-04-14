c879263441d943b6c67f9de9115d489b79fcd5af
C PROGRAMMING <121106777+Sarthak-Create@users.noreply.github.com>
2023-04-14 18:47:12 +0530

    Add files via upload

---
+++ TRASVERSAL/Trasversal.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+int arr[5] = {10 , 20 , 30 , 40 , 50};
+int i;
+
+printf("Elements of the array are:\n");
+
+for(i = 0; i<5; i++)
+{
+printf("Element at arr[%d] is %d\n", i, arr[i]);
+}
+return 0;
+}
